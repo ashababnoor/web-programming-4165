c419c380350dcad7ba7b1fcb04004aa4019d27ac
Shabab <ashababnoor@gmail.com>
2021-08-08 14:22:40 +0600

    files added

--- practice/week-4/script.js
+++ practice/week-4/script.js
@@ -1,1 +1,27 @@
-console.log('hello')
+console.log('Hello from external JS file.');
+
+let num;
+
+//num = prompt('Enter a number');
+
+num = 10;
+let x = 5;
+num = Number(num) + x;
+
+console.log(num);
+
+let word = "Hello";
+let upper = word.toUpperCase();
+let lower = word.toLocaleLowerCase();
+
+console.log(upper, lower);
+
+let btn = document.querySelector('#btn');
+
+function printToTen() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i + 1);
+    }
+}
+
+btn.addEventListener('click', printToTen);
