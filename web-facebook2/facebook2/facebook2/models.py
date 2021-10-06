1e69540e51e2af4ffe752346e12cd0c6a27f8d98
Shabab <ashababnoor@gmail.com>
2021-10-06 13:17:56 +0600

    added final

--- web-facebook2/facebook2/facebook2/models.py
+++ web-facebook2/facebook2/facebook2/models.py
@@ -8,3 +8,10 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(default=timezone.now)
     tag = models.CharField(max_length=100)
+
+
+class Appointment(models.Model):
+    preferred_date = models.DateField()
+    doctor_name = models.CharField(max_length=150)
+    patient_name = models.CharField(max_length=150)
+    phone_no = models.CharField(max_length=15)
--- web-facebook2/facebook2/facebook2/views.py
+++ web-facebook2/facebook2/facebook2/views.py
@@ -1,7 +1,8 @@
+from django.core.checks import messages
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Post
+from .models import Appointment, Post
 
 def home(request):
     homepage = '''
@@ -60,3 +61,39 @@ def profile(request):
 def article(request, id=1):
     articlepage = '<h1> Showing you article ' + str(id) + ' </h1>'
     return HttpResponse(articlepage)
+
+
+def bookappt(request):
+    if (request.method == 'GET'):
+	    return render(request, 'apptservice/bookappt.html')
+    elif (request.method == 'POST'):
+        doctor = request.POST.get('doctor_name', '')
+        patient = request.POST.get('patient_name', '')
+        date = request.POST.get('pref_date', '')
+        phone = request.POST.get('phone_no', '')
+
+        appt = Appointment(preferred_date=date, doctor_name=doctor, patient_name=patient, phone_no=phone)
+        appt.save()
+
+        # getting the last appointment by specified doctor
+        last_appt = Appointment.objects.filter(doctor_name=doctor).order_by('-preferred_date').first()
+        # getting total appointments of specified doctor on the same date as the last appt
+        serial = Appointment.objects.filter(doctor_name=doctor).filter(preferred_date = last_appt.preferred_date).count() + 1
+
+        message = "Your serial number is " + serial
+        return HttpResponse(message)
+
+
+    if (request.method == 'GET'):
+        return render(request, 'facebook2/signin.html')
+    elif (request.method == 'POST'):
+        u = request.POST.get('user', '')
+        p = request.POST.get('pass', '')
+        user = authenticate(username=u, password=p)
+        if user is None:
+            messages.error(request, 'Invalid username or password')
+            # messages.info(request, 'extra info')
+            return render(request, 'facebook2/signin.html')
+        else:
+            login(request, user)
+            return redirect('home-page')
